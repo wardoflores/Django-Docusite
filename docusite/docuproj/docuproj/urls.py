c802f9b29c011c53ee7ae34ffb653d205dcbae2e
wardoflores <61069122+wardoflores@users.noreply.github.com>
2023-03-20 14:12:20 +0800

    Completed the Django documentation tutorial till  part 7

--- docusite/docuproj/docuproj/urls.py
+++ docusite/docuproj/docuproj/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from polls import views
+
 urlpatterns = [
+    # path('', views.home, name='home'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
